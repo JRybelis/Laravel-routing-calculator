4456ec583e06d2480714f2f451ab67d2a6b7bc80
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-03-22 17:15:57 +0200

    post method calculator

--- laravelRoutingHw/app/Http/Controllers/PostCalculatorController.php
+++ laravelRoutingHw/app/Http/Controllers/PostCalculatorController.php
@@ -8,17 +8,39 @@ use Session;
 class PostCalculatorController extends Controller
 {
     public function show() {
-        $result = Session::get('addition', 'not found');
+        $result = Session::get('result', 'not found'); // retrieves the operation result from session
 
-        Session::put('addition', null);
+        Session::put('result', null); //  the operation result
 
         return view('postCalculator.show', ['result' =>$result]);
     }
 
-    public function calculate(Request $request) {
-        $addition = $request->term1 + $request->term2;
+    public function calculate (Request $request) {
+        switch ($request->operations) {
+            case 'addition':
+                $result = $request->term1 + $request->term2;
+                break;
+            case 'subtraction':
+                $result = $request->term1 - $request->term2;
+                break;
+            case 'multiplication':
+                $result = $request->term1 * $request->term2;
+                break;
+            case 'division':
+                $result = $request->term1 / $request->term2;
+                break;
+            case 'modulo':
+                $result = $request->term1 % $request->term2;
+                break;
+            case 'exponentation':
+                $result = pow($request->term1, $request->term2);
+                break;
+            case 'nthRoot':
+                $result = gmp_root($request->term1, $request->term2);
+                break;
+        }
         
-        Session::put('addition', $addition);
+        Session::put('result', $result); //saves operation result to session
 
         return redirect()->back();
     }
--- laravelRoutingHw/resources/views/postCalculator/show.blade.php
+++ laravelRoutingHw/resources/views/postCalculator/show.blade.php
@@ -3,13 +3,25 @@
 @section('calculator')
     <h1>Calculator</h1>
 
-    <from action="{{route('doMath')}}" method="post">
-    <div>{{$result}}</div>
+    <form action="{{route('doMath')}}" method="post">
     
-    First operation term: <input type="text" name="term1">
-    Second operation term: <input type="text" name="term2">
+    First operation term: <input type="text" name="term1"><br><br>
+    Second operation term: <input type="text" name="term2"><br><br>
+
+    <label for="operations">Choose your operation type: </label>
+    <select name="operations" id="operations">
+        <option value="addition">Addition</option>
+        <option value="subtraction">Subtraction</option>
+        <option value="multiplication">Multiplication</option>
+        <option value="division">Division</option>
+        <option value="modulo">Modulo</option>
+        <option value="exponentation">Exponentation</option>
+        <option value="nthRoot">NthRoot</option>
+    </select>
     <button type="submit">Calculate</button>
     @csrf
 
     </form>
+    <br>
+    <div> Result: {{$result}}</div>
 @endsection
